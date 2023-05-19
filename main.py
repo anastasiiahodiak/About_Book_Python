5860437758fa55b86d40f4f209446f21f8a30e5f
anastasiiahodiak <127551395+anastasiiahodiak@users.noreply.github.com>
2023-05-19 14:36:33 +0300

    fixed the code lab7

--- main.py
+++
@@ -1,6 +0,0 @@
-from book import Book
-
-def main():
-    books = [Book("Harry Potter", "Rowling", "Bloomsbury Children`s", 2018, "fantasy", 6), Book(), Book.get_instance(), Book.get_instance()]
-    for book in books:
-        print(book)
